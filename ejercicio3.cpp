d2f35344199850996784f5019281161d272fa413
Alison quispe <aquispeg@unjbg.edu.pe>
2025-05-30 21:48:16 -0500

    actualiza ejercicio3: imprimir porque no es primo

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 using namespace std;
-int esprimo(int n){
-	if (n<=1) return 0;
-	for (int i=2;i*i<=n;i++){
-		if(n%i==0)return 0;
+bool esprimo(int n){
+	if(n<=1){
+		cout<<n<<"no es primo porque es menor o igual que 1"<<endl;
+		return false;
 	}
-	return 1;
+	if (n==2) return 1;
+	if (n%2==0){
+		cout<<n<<" no es primo porque es divisible por 2"<<endl;
+		return true;
+	}
+	for (int i=3;i*i<=n;i= i+2){
+		if(n&i ==0){
+			cout<<n<<"no es primo porque es divisible por "<<i<<" . "<<endl;
+			return false;
+		}
+	}
+	return true;
+	
 }
 int main(){
 	int numero;
 	cout<<"ingrese un numero entero positivo: ";
 	cin>>numero;
 	if (esprimo(numero)){
-		cout<<numero<<" es un numero primo."<<endl;		
-	}else{
-		cout<<numero<<" no es un numero primo"<<endl;
+		cout<<numero<< " es un numero primo. "<<endl;
+	} else {
+		
 	}
-	return 0;
+	return false;
 }
