59e17e576dc5fcc73ab4831ea2de416bc4e7abc2
Alison quispe <aquispeg@unjbg.edu.pe>
2025-05-31 21:34:25 -0500

    actualiza ejercicio4: validacion de datos de los indices

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -5,6 +5,10 @@ int main(){
 	int m,n,c;
 	cout<<"ingrese los indices de la combinatoria: "<<endl;
 	cin>>m>>n;
+	if (n>m || m<0 || n<0){
+		cout<<"error: asegurese de que m>=n y ambos sean numeros no negativos"<<endl;
+		return 1;
+	}
 	c=(factorial(m))/((factorial(n)*factorial(m-n)));
 	cout<<"el valor de la combinatoria es"<<endl
 	    <<c;
